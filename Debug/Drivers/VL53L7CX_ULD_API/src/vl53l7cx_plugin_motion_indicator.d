f874c411580f6bd993e4b2af414d1b778817a658
OLiwerFig <259754@student.pwr.edu.pl>
2024-07-18 15:47:17 +0200

    Poprawic

--- Debug/Drivers/VL53L7CX_ULD_API/src/vl53l7cx_plugin_motion_indicator.d
+++
@@ -1,8 +0,0 @@
-Drivers/VL53L7CX_ULD_API/src/vl53l7cx_plugin_motion_indicator.o: \
- ../Drivers/VL53L7CX_ULD_API/src/vl53l7cx_plugin_motion_indicator.c \
- /Users/oliwerfigura/STM32CubeIDE/workspace_1.15.1/Inzynierka/Drivers/VL53L7CX_ULD_API/inc/vl53l7cx_plugin_motion_indicator.h \
- /Users/oliwerfigura/STM32CubeIDE/workspace_1.15.1/Inzynierka/Drivers/VL53L7CX_ULD_API/inc/vl53l7cx_api.h \
- /Users/oliwerfigura/STM32CubeIDE/workspace_1.15.1/Inzynierka/Drivers/VL53L7CX_ULD_API/inc/platform.h
-/Users/oliwerfigura/STM32CubeIDE/workspace_1.15.1/Inzynierka/Drivers/VL53L7CX_ULD_API/inc/vl53l7cx_plugin_motion_indicator.h:
-/Users/oliwerfigura/STM32CubeIDE/workspace_1.15.1/Inzynierka/Drivers/VL53L7CX_ULD_API/inc/vl53l7cx_api.h:
-/Users/oliwerfigura/STM32CubeIDE/workspace_1.15.1/Inzynierka/Drivers/VL53L7CX_ULD_API/inc/platform.h:
